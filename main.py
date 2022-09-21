84a6576a0397119816ac594c43cd726e265ca835
DeadlyCucumber06 <78033807+DeadlyCucumber06@users.noreply.github.com>
2022-09-21 19:10:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 high = (110, 255, 255)
 low = (0, 130, 130)
-
-blue = (0, 0, 255)
 
 video = cv2.VideoCapture('task.mp4')
 
@@ -13,27 +10,30 @@ while(video.isOpened()):
   #plays and filters for yellow only
   ret, frame = video.read()
   mask = cv2.inRange(frame, low, high)
-  #cv2.imshow('mask', mask)
+  cv2.line(frame, (0, 0), (511, 511), (255, 0, 0), 5)
+  cv2.imshow("original", frame)
   
   #blur for better canny effect
   blurred = cv2.GaussianBlur(mask,(7,7),0)
-  #cv2.imshow('blur', blurred)
+  cv2.imshow('blur', blurred)
 
   #finds edge of the line
   edges = cv2.Canny(blurred, 50, 150)
-  #cv2.imshow("edges",edges)
 
-  kernel = np.ones((5,5),np.uint8)
-  erosion = cv2.erode(mask,kernel,iterations = 8)
+  cv2.imshow('Blended Image', edges)
+
+  #kernel = np.ones((5,5),np.uint8)
+  #erosion = cv2.erode(mask,kernel,iterations = 8)
 
 
   #cv2.imshow("erode", erosion)
 
-  combined = cv2.addWeighted(edges,0.5,erosion,0.7,0)
-  cv2.imshow('Blended Image',combined)
+  #combined = cv2.addWeighted(edges,0.5,erosion,0.7,0)
+  #cv2.line(combined, (0, 0), (511, 511), (0, 0, 255), 5)
+
 
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
 
-
+  
 cv2.destroyAllWindows()
