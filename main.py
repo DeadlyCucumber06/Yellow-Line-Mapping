12d98504a369a50f0eaf485d418d8a927963e58b
DeadlyCucumber <kevinsiop2a@gmail.com>
2022-09-27 19:23:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,28 +11,44 @@ while(video.isOpened()):
   #plays and filters for yellow only
   ret, frame = video.read()
   mask = cv2.inRange(frame, low, high)
-  #cv2.imshow("original", frame)q
+  #cv2.imshow("original", frame)
   
   #blur for better canny effect
-  blurred = cv2.GaussianBlur(mask,(31,31),0)
+  blurred = cv2.GaussianBlur(mask,(7,7),0)
   cv2.imshow('blur', blurred)
 
   #finds edge of the line
   edges = cv2.Canny(blurred, 50, 150)
   
-  bluredges = cv2.GaussianBlur(edges,(31,31),0)
+  bluredges = cv2.GaussianBlur(edges,(7,7),0)
 
   cv2.imshow('Blended Image', bluredges)
     
-  linesP = cv2.HoughLinesP(bluredges, 1, np.pi / 180, 50, 1000 , 1000, 5000)
+  """linesP = cv2.HoughLinesP(bluredges, 1, np.pi / 180, 50, 1000 , 1000, 5000)
   if linesP is not None:
     for i in range(0, len(linesP)):
       l = linesP[i][0]
       cv2.line(frame, (l[0], l[1]), (l[2], l[3]), (0,0,255), 1, cv2.LINE_AA)
       print(l[0], l[1], l[2], l[3])
-      templ = linesP[i][0]
+      templ = linesP[i][0]"""
+      
+  lines = cv2.HoughLines(bluredges, 1, np.pi/180, 325, None, 0 ,0)
+  if lines is not None:
+    for i in range(0, len(lines)):
+      rho = lines[i][0][0]
+      theta = lines[i][0][1]
+      a = math.cos(theta)
+      b = math.sin(theta)
+      x0 = a * rho
+      y0 = b * rho
+      pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+      pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
 
-  cv2.imshow("stuff", frame)
+      cv2.line(frame, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
+      cv2.imshow("stuff", frame)
+      print("rho", rho, "   theta", theta)
+
+  #cv2.imshow("stuff", frame)
   
   #erosion = cv2.erode(mask,kernel,iterations = 8)
 
